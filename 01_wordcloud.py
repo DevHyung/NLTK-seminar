b9aa0ff37a7c26eb88043f9a31a6f878337ef6a8
DevHyung <khuphj@gmail.com>
2018-08-16 13:17:09 +0900

    Add file

---
+++ 01_wordcloud.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+# Version: 1.0
+'''
+    File name: 01_wordcloud.py
+    Author: DevHyung
+    Date created: 2018.08.16
+    Date last modified: SAME
+    Python Version: 3.6
+    Description :
+        워드클라우드파일
+'''
+
+if __name__ == "__main__":
+    print('gd')
